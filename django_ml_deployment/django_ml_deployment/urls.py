e1d20cc235f4ac10964f62cf3d2b1dc514596464
Admin <thakerniramay@gmail.com>
2024-02-02 14:49:50 +0530

    Creating ML model

--- django_ml_deployment/django_ml_deployment/urls.py
+++ django_ml_deployment/django_ml_deployment/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("main.urls"))
+    # path("", include("main.urls"))
+    path("", include("iris_app.urls"))
 ]
